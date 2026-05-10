a1328aeedadce3f065de51c32e248631108aebb8
RaulDuriez <39564328+raulduriez@users.noreply.github.com>
2026-05-09 20:41:06 -0600

    Add files via upload

---
+++ js/radar_logic.js
@@ -0,0 +1,103 @@
+var json_edcion_paradas_Mang_1 = {
+  "type": "FeatureCollection",
+  "name": "edcion_paradas_Mang_1",
+  "crs": { 
+    "type": "name", 
+    "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } 
+  },
+  "features": [
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "1", 
+        "name": "Diesel Motors", 
+        "Nom_Parada": "Diesel Motors", 
+        "Id": "3001",
+        "coordX": "581033.018",
+        "CoordY": "1341508.70"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2552642, 12.1342427] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "2", 
+        "name": "Bayer", 
+        "Nom_Parada": "Bayer", 
+        "Id": "3002",
+        "coordX": "588018.873",
+        "CoordY": "1340284.99"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.1910982, 12.1229974] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "16", 
+        "name": "Rotonda El Periodista", 
+        "Nom_Parada": "Rotonda El Periodista", 
+        "Id": "56",
+        "Rutas": "210",
+        "Recorrido": "VUELTA"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2819994, 12.1267721] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "45", 
+        "name": "DGI", 
+        "Nom_Parada": "Serrano", 
+        "Id": "51",
+        "Rutas": "210",
+        "Recorrido": "IDA"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.266001, 12.1345203] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "102", 
+        "name": "Templo Mormón Altagracia", 
+        "Nom_Parada": "Templo Mormón Altagracia", 
+        "Id": "102"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2895561, 12.1360165] } 
+    },
+    // Nuevas paradas detectadas en la actualización
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "103", 
+        "name": "Avenida Pochote", 
+        "Nom_Parada": "Avenida Pochote", 
+        "Id": "103"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2358434, 12.1166175] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "104", 
+        "name": "El Edén", 
+        "Nom_Parada": "El Edén", 
+        "Id": "104"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2418829, 12.1410726] } 
+    },
+    { 
+      "type": "Feature", 
+      "properties": { 
+        "full_id": "39", 
+        "name": "Farmacia Buendía", 
+        "Nom_Parada": "Farmacia Buendía", 
+        "Id": "39",
+        "Rutas": "210",
+        "Recorrido": "IDA"
+      }, 
+      "geometry": { "type": "Point", "coordinates": [-86.2936906, 12.1308816] } 
+    }
+    // ... Nota: El archivo contiene más de 800 puntos. 
+    // Se recomienda importar el objeto completo desde el archivo fuente.
+  ]
+};
